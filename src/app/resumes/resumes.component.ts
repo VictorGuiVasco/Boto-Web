d1b3a3cd7620b90d089a63366e6f85c0c3b02295
victorguivasco <victorguilhermecavalcante@gmail.com>
2024-02-12 23:19:28 -0400

    feat: create list resumes page

--- src/app/resumes/resumes.component.ts
+++ src/app/resumes/resumes.component.ts
@@ -1,12 +1,43 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resumes',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './resumes.component.html',
-  styleUrl: './resumes.component.css'
+  styleUrl: './resumes.component.css',
 })
 export class ResumesComponent {
-
+  timeFilter = [
+    {
+      option: '0 a 1 ano',
+    },
+    {
+      option: '1 a 3 ano',
+    },
+    {
+      option: '3 a 5 ano',
+    },
+    {
+      option: 'A partir de 5 anos',
+    },
+  ];
+  educationFilter = [
+    {
+      option: 'Ensino médio',
+    },
+    {
+      option: 'Ensino superior',
+    },
+    {
+      option: 'Pós graduação',
+    },
+    {
+      option: 'Mestrado',
+    },
+    {
+      option: 'Doutorado',
+    },
+  ];
 }
