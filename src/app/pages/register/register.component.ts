2fc3e64efd7a088feff09ed0842129be1c30c87a
victorguivasco <victorguilhermecavalcante@gmail.com>
2024-03-13 20:31:31 -0400

    feat: update form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import { AxiosService } from '../../../libs/axios.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  nome = new FormControl('');
+  fullText = '';
+  formData = new FormData();
 
+  constructor(private apiService: AxiosService) {}
+
+  updateFile(event: any) {
+    const file = event.target.files[0];
+    this.formData.append('file', file);
+  }
+
+  uploadData() {
+    this.apiService.post({
+      nome: this.nome.value ?? '',
+      fullText: this.fullText,
+    });
+  }
 }
--- src/libs/axios.service.ts
+++ src/libs/axios.service.ts
@@ -14,4 +14,8 @@ export class AxiosService {
   getCompleteResume() {
     return api.get('/completeResume');
   }
+
+  post(data: { nome: string; fullText: string }) {
+    return api.post('/pessoa', data);
+  }
 }
